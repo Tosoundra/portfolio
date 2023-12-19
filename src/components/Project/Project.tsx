f98a4a9719f7b8c0ddf70516b96689e24fcfea84
Antony Kowalski <beheaded_96@mail.ru>
2023-12-19 16:30:30 +0700

    test commit

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,29 +1,19 @@
 import { FC } from 'react';
-import { ProjectStyled, TitleComponent } from './ProjectStyled';
+import { ProjectLogoStyled, ProjectStyled, TitleComponent } from './ProjectStyled';
 import { Link } from 'react-router-dom';
 
 interface Props {
   title: string;
   image: string;
   link: string;
-  activeCurrent: string;
-  onClick: (title: string) => void;
 }
 
-export const Project: FC<Props> = ({ title, image, link, activeCurrent, onClick }) => {
-  const onClickHandle = () => {
-    if (activeCurrent === title) {
-      onClick('');
-    } else {
-      onClick(title);
-    }
-  };
-
+export const Project: FC<Props> = ({ title, image, link }) => {
   return (
     <Link to={link} type="button" target="_blank">
-      <ProjectStyled onClick={onClickHandle} image={image} $isActive={activeCurrent === title}>
+      <ProjectStyled direction="row">
         <TitleComponent>{title}</TitleComponent>
-        <img src={image} alt="img" width={50} height={50} />
+        <ProjectLogoStyled src={image} width={50} height={50} alt="project logo" />
       </ProjectStyled>
     </Link>
   );
--- src/components/User/UserStyledComponent.tsx
+++ src/components/User/UserStyledComponent.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { FlexComponent } from '../../styledComponents/FlexComponent/FlexComponent';
 import { WrapperDescription } from '../../styledComponents/Wrapper/Wrapper';
+import { ImageStyled } from '../../styledComponents/ImageStyled/ImageStyled';
 
 export const UserInfoContainer = styled(WrapperDescription)`
   display: grid;
@@ -25,4 +26,11 @@ export const UserOccupationTextStyle = styled.span`
   font-family: 'Inter Medium';
   font-size: 18px;
   line-height: 20px;
+`;
+
+export const UserPhotoStyled = styled(ImageStyled)`
+  border-radius: 10px;
+
+  grid-column: 2;
+  grid-row: 1 / -3;
 `;
