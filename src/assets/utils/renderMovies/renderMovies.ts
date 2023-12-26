5d5e23bd673a6e5edf1413e73cf51bbeb627ce94
Antony Kowalski <beheaded_96@mail.ru>
2023-12-26 16:10:08 +0700

    fix function

--- src/assets/utils/renderMovies/renderMovies.ts
+++ src/assets/utils/renderMovies/renderMovies.ts
@@ -13,15 +13,11 @@ export const renderMovies: RenderMoviesType = (
   searchingMovie,
   countMovie,
 ) => {
-  try {
-    const forRender = showShortMovie ? movies.filter((movie) => movie.duration < 40) : movies;
+  const forRender = showShortMovie ? movies.filter((movie) => movie.duration < 40) : movies;
 
-    return forRender
-      .slice(0, countMovie)
-      .filter((movie) =>
-        movie.nameRU.trim().toLocaleLowerCase().includes(searchingMovie.toLocaleLowerCase()),
-      );
-  } catch (error) {
-    throw error;
-  }
+  return forRender
+    .slice(0, countMovie)
+    .filter((movie) =>
+      movie.nameRU.trim().toLocaleLowerCase().includes(searchingMovie.toLocaleLowerCase()),
+    );
 };
